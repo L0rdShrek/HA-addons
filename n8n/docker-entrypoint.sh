466c7da736e806d7f75180d625ad0ef4d74d5bcb
Florian Niefünd <admin@l0rdshrek.info>
2022-11-29 19:46:08 +0000

    test

--- n8n/docker-entrypoint.sh
+++ n8n/docker-entrypoint.sh
@@ -79,8 +79,8 @@ if [ -d ${N8N_PATH_LOCAL} ] ; then
 fi
 
 chmod -R 777 /usr/local/lib/node_modules/n8n
-chmod -R 777 /home/node
-chown -R node /home/node
+#chmod -R 777 /home/node
+#chown -R node /home/node
 
 if [ "$#" -gt 0 ]; then
   echo "Got started with arguments"
